35da9e98c6e9722f94c184a0ba1616486fad20c8
Marwa Majidah <marwa.nbstar@gmail.com>
2019-11-29 05:15:07 -0800

    Add files via upload

---
+++ application/views/customer/beranda.php
@@ -0,0 +1,5 @@
+
+<div class="jumbotron text-center">
+  <h1>Arini Apotik</h1>
+  <p>"Beli obat hanya di Arini Apotik yang terpercaya"</p>
+</div>
---
+++ application/views/home/beranda.php
@@ -0,0 +1,5 @@
+
+<div class="jumbotron text-center">
+  <h1>Arini Apotek</h1>
+  <p>Situs Pembelian Obat Terpercaya</p>
+</div>
